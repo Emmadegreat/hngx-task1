39f70992418e2026c9f49ce98ac97db463351813
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2023-09-08 14:17:19 +0100

    add

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("copyright").innerHTML = new Date().getFullYear();
+//document.getElementById("copyright").innerHTML = new Date().getFullYear();
 let currentDay = document.getElementById("current_day");
 
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -14,3 +14,4 @@ let seconds = date.getUTCSeconds();
 
 let currentUTC = document.getElementById("current_utc");
 currentUTC.innerHTML = date.getTime();
+
