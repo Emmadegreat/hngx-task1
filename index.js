34ade0404a59af439278b6e1ab95c60775192203
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2023-09-08 10:40:35 +0100

    add

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ let date = new Date();
 let day = weekDays[date.getDay()];
 currentDay.innerHTML = day;
 
+let hour = date.getUTCHours();
+let minutes = date.getUTCMinutes();
+let seconds = date.getUTCSeconds();
+
+
 let currentUTC = document.getElementById("current_utc");
-currentUTC.innerHTML = date.getTime();
+currentUTC.innerHTML = `${hour.toString().padStart(2, '0')} :
+                        ${minutes.toString().padStart(2, '0')} :
+                        ${seconds.toString().padStart(2, '0')}`
